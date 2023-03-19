83ff69b93c5252e2f951d16df4490ca465363068
doubleaykay <the.a.cars@gmail.com>
2023-03-19 12:52:35 -0400

    update webpack to compile typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/logic.js',
+  entry: './src/logic.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: 'logic.js',
     path: path.resolve(__dirname, 'dist'),
