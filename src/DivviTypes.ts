191e0c7c154fb5c2051f1c6c36453914708ae84d
doubleaykay <the.a.cars@gmail.com>
2023-11-20 19:46:53 -0500

    temporarily comment out types not being used

--- src/DivviTypes.ts
+++ src/DivviTypes.ts
@@ -29,12 +29,12 @@ export type TipTotalDollars = {
 }
 
 // data structure for a person
-export type Person = {
-    contribution_pre_tax: Dinero<number>;
-    pay_type: PayType;
-    contribution_pct_ideal: number;
-    contribution_calculated: Dinero<number> | undefined;
-}
+// export type Person = {
+//     contribution_pre_tax: Dinero<number>;
+//     pay_type: PayType;
+//     contribution_pct_ideal: number;
+//     contribution_calculated: Dinero<number> | undefined;
+// }
 
 // enum for tip calculation method for a given bill
 export enum TipType {
@@ -47,37 +47,37 @@ export enum TipType {
 // data structure for the bill
 // timestamp format: 'Sun Mar 19 2023 12:04:00 GMT-0400 (Eastern Daylight Time)'
 // if tip_type is TipDollars or TotalDollars then tip_computed_amt will be computed when needed
-export type Bill = {
-    id: string;
-    timestamp: string;
-    people: {
-        [key: string]: Person
-    };
-    tax_amt: Dinero<number>;
-    tip_type: TipType;
-    tip_pct_requested: number | undefined;
-    tip_amt_requested: Dinero<number> | undefined;
-    tip_amt_computed: Dinero<number> | undefined;
-    total_pre_tax: Dinero<number> | undefined;
-    total: Dinero<number> | undefined;
-}
+// export type Bill = {
+//     id: string;
+//     timestamp: string;
+//     people: {
+//         [key: string]: Person
+//     };
+//     tax_amt: Dinero<number>;
+//     tip_type: TipType;
+//     tip_pct_requested: number | undefined;
+//     tip_amt_requested: Dinero<number> | undefined;
+//     tip_amt_computed: Dinero<number> | undefined;
+//     total_pre_tax: Dinero<number> | undefined;
+//     total: Dinero<number> | undefined;
+// }
 
 // Person type for data from frontend
-type PersonData = {
+export type PersonData = {
     contribution_pre_tax: Dinero<number>;
     pay_type: PayType;
     contribution_pct_ideal: number;
 }
 
 // Person type for computed result
-type PersonResult = {
+export type PersonResult = {
     // contribution_pre_tax: Dinero<number>;
     // pay_type: PayType;
     contribution_pct_ideal: number;
     contribution_calculated: Dinero<number>;
 }
 
-type BillInputData = {
+export type BillInputData = {
     timestamp: string;
     people: {
         [key: string]: PersonData
@@ -88,7 +88,7 @@ type BillInputData = {
 
 // TODO Person still has an undefined field. is there a good way to fix that?
 // TODO do we need to retain tip type information for the results view?
-type BillResult = {
+export type BillResult = {
     timestamp: string;
     people: {
         [key: string]: PersonResult
