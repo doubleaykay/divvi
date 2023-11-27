173b7390a6437c6a9dd68bfe98b6ab42f91906dd
doubleaykay <the.a.cars@gmail.com>
2023-11-26 21:36:04 -0500

    start basic React frontend to test DivviLogic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,56 @@
 import React from 'react';
+import { FormEvent } from "react";
 
 function App() {
+  function compute(e: FormEvent<HTMLFormElement>) {
+    // prevent refreshing of the entire page on form submit
+    e.preventDefault()
+
+    // get data contained in form as JSON
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    console.log(formJson);
+
+    // convert form data to BillData type object and run through processing
+  }
+
   return (
     <div className="App">
       Hello World!
 
-      {/* multiple people */}
+      <form onSubmit={compute} method="POST">
 
-      {/* tax amount */}
+        {/* multiple people */}
+        <hr />
+        People go here...
+        <button type="button">Add person</button>
 
-      {/* tip type */}
-      {/* tip value */}
+        {/* tax amount */}
+        <hr />
+        Tax:
+        <input name="taxValue" />
 
-      {/* compute button */}
+        {/* tip type */}
+        <hr />
+        Tip type:
+        <select name="tipType">
+          <option value="someOption">Some option</option>
+          <option value="otherOption">Other option</option>
+        </select>
+        {/* tip value */}
+        <br />
+        Tip value:
+        <input name="tipValue" />
 
-      {/* results view */}
+        {/* compute button */}
+        <hr />
+        <button type="submit">Compute</button>
+
+        {/* results view */}
+
+      </form>
     </div>
   );
 }
